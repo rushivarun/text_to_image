0a1f36498c5b9086dedc0e242fe53a85ddde438e
Rushi Varun <rushi2000varun@gmail.com>
2020-02-23 22:58:54 +0530

    chan

--- aarya/src/App.js
+++ aarya/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import New_Main from './Pages/New_Main/New_Main';
+import New_Main from './Pages/Main/Main';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <New_Main/>
+        <Main/>
       </div>
     );
   }
