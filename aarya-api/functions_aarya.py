f8709424b04a4c9a8a7052fe400eb435dfbb3917
Rushi Varun <rushi2000varun@gmail.com>
2020-02-27 19:39:16 +0530

    efea

--- aarya-api/functions_aarya.py
+++ aarya-api/functions_aarya.py
@@ -1,9 +1,10 @@
 from rake_nltk import Rake
 import requests
 import nltk
+from bs4 import BeautifulSoup
+import urllib
+import json
 
-nltk.download('stopwords')
-nltk.download('punkt')
 
 subscription_key = "aa5e41a6700248c1a949ee8dd42e682f"
 search_url = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
@@ -27,3 +28,36 @@ def FetchImage(search_term):
     thumbnail_urls = [img["thumbnailUrl"] for img in search_results["value"][:1]]
     return {'image_url': thumbnail_urls, 'search_word': search_term}
 
+
+
+query = input('input your query here.. - ')
+text_plus = query.replace(' ', '+')
+link = 'https://www.wikihow.com/wikiHowTo?search='+text_plus
+result = requests.get(link)
+
+# if successful parse the download into a BeautifulSoup object, which allows easy manipulation 
+if result.status_code == 200:
+    soup = BeautifulSoup(result.content, "html.parser")
+# print(soup)
+print('----------------------------------------------------------------------------------------------------------------')
+search_links = []
+for i in soup.findAll('a'):
+    search_links.append(i.get('href'))
+print(search_links)
+print('----------------------------------------------------------------------------------------------------------------')
+print(search_links[30])
+result = requests.get(search_links[29])
+
+# if successful parse the download into a BeautifulSoup object, which allows easy manipulation 
+if result.status_code == 200:
+    soup = BeautifulSoup(result.content, "html.parser")
+images = []
+for img in soup.findAll('img'):
+    images.append(img.get('src'))
+imp_images = images[:24]
+image_links = imp_images[2::2]
+
+print({
+    'step':1,
+    'links' : image_links
+})
